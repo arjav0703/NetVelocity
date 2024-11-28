96d7c8891acc22a4adb0ef6b737763adbc812abb
arjav0703 <arjavjain0703@gmail.com>
2024-11-28 20:18:51 +0530

    update main

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 from tkinter import * 
 import speedtest
 
+window = Tk()
+window.geometry('360x600')
+window.resizable(False,False)
+
+window.configure(bg='#1a212d')
+window.title('NetVelocity')
+
+window.mainloop()
